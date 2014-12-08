fc77c25d62d33e14fc56819b4a195ee80331b8b1
ckib16 <ckib16@gmail.com>
2014-12-08 18:15:20 -0500

    finished #31 Associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
 require 'faker'
 
+#Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 #Create Posts
 
 50.times do
   Post.create!(
+    user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
     )
@@ -13,10 +26,18 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
+   #user: users.sample,  #we havn't acssocaited user with comments yet 
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'ckib16@gmail.com',
+  password: 'helloworld'
+)
 
 puts "Seed Finished"
 puts "#{Post.count} posts created"
