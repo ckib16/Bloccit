2d9da3cc591a97e64f6a845ba5afd5a1c3317b7d
ckib16 <ckib16@gmail.com>
2014-12-26 23:27:08 -0500

    #38 complete - added image uploading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226213733) do
+ActiveRecord::Schema.define(version: 20141227033420) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141226213733) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -62,6 +63,7 @@ ActiveRecord::Schema.define(version: 20141226213733) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
