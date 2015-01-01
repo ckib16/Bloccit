a2eaf632db8e1e9b76faf145ee10f44f2d1a51dc
ckib16 <ckib16@gmail.com>
2015-01-01 16:31:49 -0500

    L44 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -31,12 +31,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5}, presence: true
   validates :body, length: { minimum: 20}, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
-
-  after_create :create_vote
-
-  private
+  validates :topic, presence: true
+  validates :user, presence: true
 
   def create_vote
     user.votes.create(value:1, post: self)
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Vote do
 
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
@@ -15,5 +17,15 @@ describe Vote do
         expect(v3.valid?).to eq(false)
       end
     end
+
+    describe 'after-save' do
+      it "calls 'Post#update_rank' after save" do
+        post = associated_post
+        vote = Vote.new(value: 1, post: post)
+        expect(post).to receive(:update_rank)
+        vote.save
+      end
+    end
+
   end      
 end
