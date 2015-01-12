79385f024a4f283a941e7480b6d45672e5b76c37
ckib16 <ckib16@gmail.com>
2015-01-11 21:22:50 -0500

    tests passing, still have questions

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A generated commment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+    @comment = comment_without_email(post: @post, user: @user)
+    
   end
 
   describe "not signed in" do
@@ -21,6 +22,20 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
     end
-
   end
+
+  describe "when user is signed in" do
+
+    before { login_as(@user, :scope => :user) }
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
 end
+
